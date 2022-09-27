e47f467cf40b2ac310901683a851a856bfd3df9d
Piero <piero.pisfil.p@uni.pe>
2022-09-26 20:38:24 -0500

    Se agregó el manifest y el init

---
+++ modelos-fotocopiadoras/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "module-fotocopier",
+    'summary': "Modulo personalizado para agregar modelos de fotocopiadoras en Odoo",
+    'version': '1.1',
+    'description': """
+       Fotocopier's data
+    """,
+    'author': "Piero Pisfil",
+    'depends': [
+        'base',
+        'stock',
+    ],
+    'data': [
+    ],
+    'license': 'LGPL-3',
+}
