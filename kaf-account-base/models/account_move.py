495e04ac3096efc653bcf2f2b4f94466ffadf0d1
Piero <piero.pisfil.p@uni.pe>
2022-11-18 11:18:12 -0500

    mejoro la secuencia

--- kaf-account-base/models/account_move.py
+++ kaf-account-base/models/account_move.py
@@ -4,6 +4,7 @@ from odoo import api, fields, models, _
 # from datetime import datetime
 from collections import defaultdict
 import json 
+from odoo.tools.misc import format_date
 # import re
 # from . import amount_to_text_es
 from odoo.exceptions import UserError, ValidationError
@@ -92,6 +93,33 @@ class AccountMove(models.Model):
             batch['records']._compute_split_sequence()
 
         self.filtered(lambda m: not m.name).name = '/'
+
+    def _constrains_date_sequence(self):
+        constraint_date = fields.Date.to_date(self.env['ir.config_parameter'].sudo().get_param(
+            'sequence.mixin.constraint_start_date',
+            '1970-01-01'
+        ))
+        for record in self:
+            if record.journal_id.usar_secuencia_propia:
+                continue
+            date = fields.Date.to_date(record[record._sequence_date_field])
+            sequence = record[record._sequence_field]
+            if sequence and date and date > constraint_date:
+                format_values = record._get_sequence_format_param(sequence)[1]
+                if (
+                    format_values['year'] and format_values['year'] != date.year % 10**len(str(format_values['year']))
+                    or format_values['month'] and format_values['month'] != date.month
+                ):
+                    raise ValidationError(_(
+                        "The %(date_field)s (%(date)s) doesn't match the sequence number of the related %(model)s (%(sequence)s)\n"
+                        "You will need to clear the %(model)s's %(sequence_field)s to proceed.\n"
+                        "In doing so, you might want to resequence your entries in order to maintain a continuous date-based sequence.",
+                        date=format_date(self.env, date),
+                        sequence=sequence,
+                        date_field=record._fields[record._sequence_date_field]._description_string(self.env),
+                        sequence_field=record._fields[record._sequence_field]._description_string(self.env),
+                        model=self.env['ir.model']._get(record._name).display_name,
+                    ))
 
     def _post(self, soft=True):
         for invoice_id in self:
