a99d0c5cfdf436731a534145149b3a5dfb322356
PieroPisfil <piero.pisfil.p@uni.pe>
2022-12-05 21:35:34 -0500

    se agrego contado credito en pos

--- kaf-pos-base/static/js/Screens/ProductScreen/ControlButton/SetMetodoPagoButton.js
+++ kaf-pos-base/static/js/Screens/ProductScreen/ControlButton/SetMetodoPagoButton.js
@@ -27,9 +27,10 @@ odoo.define('kaf-pos-base.SetMetodoPagoButton', function(require) {
                 : this.env._t('Método de Pago');
         }
         async onClick() {
+            var self = this;
             const currentMetodoPago = this.currentOrder.forma_de_pago_pe;
             const metodopagoPosList = [
-                {
+/*                 {
                     id: this.currentOrder.forma_de_pago_pe_alt[0].id,
                     label: this.currentOrder.forma_de_pago_pe_alt[0].name,
                     isSelected: currentMetodoPago ? this.currentOrder.forma_de_pago_pe_alt[0].id === currentMetodoPago.id
@@ -49,8 +50,17 @@ odoo.define('kaf-pos-base.SetMetodoPagoButton', function(require) {
                     isSelected: currentMetodoPago ? this.currentOrder.forma_de_pago_pe_alt[2].id === currentMetodoPago.id
                     : false,
                     item : this.currentOrder.forma_de_pago_pe_alt[2],
-                },
+                }, */
             ];
+            for (let element of this.currentOrder.forma_de_pago_pe_alt){
+                metodopagoPosList.push({
+                    id: element.id,
+                    label: element.name,
+                    isSelected: currentMetodoPago ? element.id === currentMetodoPago.id
+                    : false,
+                    item: element,
+                })
+            }
             const { confirmed, payload: selectedMetodoPago } = await this.showPopup(
                 'SelectionPopup',
                 {
@@ -59,8 +69,20 @@ odoo.define('kaf-pos-base.SetMetodoPagoButton', function(require) {
                 }
             );
             if (confirmed) {
-                console.log(selectedMetodoPago);
-                this.currentOrder.forma_de_pago_pe = this.currentOrder.forma_de_pago_pe_alt[selectedMetodoPago.id];
+                //console.log(selectedMetodoPago);
+                this.currentOrder.forma_de_pago_pe = selectedMetodoPago;
+                if(this.currentOrder.forma_de_pago_pe.code === 'GARANTIA'){
+                    for (let line of this.currentOrder.orderlines.models) {
+                        line.set_unit_price(0);
+                    }
+                }
+                else{
+                    for (let line of this.currentOrder.orderlines.models) {
+                        line.set_unit_price(line.product.get_price(this.currentOrder.pricelist, line.get_quantity(), line.get_price_extra()));
+                    }
+                    //this.currentOrder.set_pricelist(this.currentOrder.pricelist;
+                }
+
                 this.currentOrder.trigger('change');
             }
         }
