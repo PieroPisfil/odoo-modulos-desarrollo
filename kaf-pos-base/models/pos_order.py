deed0d7e7db51938d991bba4055f6f1bd5a8039b
PieroPisfil <piero.pisfil.p@uni.pe>
2022-12-10 20:49:23 -0500

    mejorass

--- kaf-pos-base/models/pos_order.py
+++ kaf-pos-base/models/pos_order.py
@@ -1,14 +1,18 @@
-# -*- encoding: utf-8 -*-
-from odoo import api, fields, models, _
+# -*- coding: utf-8 -*-
+
+from odoo import fields, api, models
 from odoo.exceptions import ValidationError
 import logging
 
+
 _logging = logging.getLogger(__name__)
 
+
 class PosOrder(models.Model):
-	_inherit = "pos.order"
+    _inherit = "pos.order"
     
-    number_doc_relacionado = fields.Char(string='#Doc. Relacionado', readonly=True, copy=False)
+     
+    numero_doc_relacionado = fields.Char(string='Doc. Relacionado', readonly=True, copy=False)
     invoice_sequence_number = fields.Integer(string='Secuencia de números de factura', readonly=True, copy=False)
     invoice_journal = fields.Many2one('account.journal', string='Diario de facturas de ventas',   states={'draft': [('readonly', False)]}, readonly=True, domain="[('type', 'in', ['sale'])]", copy=True)
     date_invoice = fields.Date("Fecha de la factura")
