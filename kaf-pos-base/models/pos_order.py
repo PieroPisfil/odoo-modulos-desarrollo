9749b29deb0f573082a16ac67b9c45465071f634
PieroPisfil <piero.pisfil.p@uni.pe>
2022-12-12 21:55:30 -0500

    se hizo la facturacion en pos

--- kaf-pos-base/models/pos_order.py
+++ kaf-pos-base/models/pos_order.py
@@ -1,15 +1,52 @@
 # -*- coding: utf-8 -*-
 
-from odoo import fields, api, models
+from odoo import fields, api, tools, models
+from datetime import datetime
 from odoo.exceptions import ValidationError
+import pytz
+import psycopg2
+from odoo.tools import float_is_zero, float_round, float_repr, float_compare
 import logging
+from dateutil.parser import parse as parse_date
+from odoo.tools import DEFAULT_SERVER_DATE_FORMAT as DATE_FORMAT
 
+tz = pytz.timezone('America/Lima')
 
 _logging = logging.getLogger(__name__)
+_logger = logging.getLogger(__name__)
 
 
 class PosOrder(models.Model):
     _inherit = "pos.order"
+
+    @api.model
+    def _get_journal(self, id_journal):
+        return self.env['account.journal'].search([
+                ('id', '=', id_journal), 
+                ('active', '=', True)
+            ], limit=1).id
+
+    @api.model
+    def _order_fields(self, ui_order):
+        res = super(PosOrder, self)._order_fields(ui_order)
+        res['invoice_journal'] = ui_order.get('journal_id', False)
+        self.invoice_journal = self._get_journal(res['invoice_journal']) if res['invoice_journal'] else None
+        _logger.warning('////////******************************////////////// {0}'.format(self.invoice_journal))
+        reg_datetime = datetime.now(tz)
+        fecha = reg_datetime.strftime("%Y-%m-%d")
+        res['date_invoice'] = parse_date(ui_order.get('date_invoice', fecha)).strftime(DATE_FORMAT)
+        return res
+    	
+    def _prepare_invoice_vals(self):
+        res = super(PosOrder, self)._prepare_invoice_vals()
+        timezone = pytz.timezone(self._context.get('tz') or self.env.user.tz or 'UTC')
+        res['invoice_date'] = self.date_invoice or self.date_order.astimezone(timezone).date()
+        # if not res.get('name') and res.get('type') == 'out_refund':
+        #     res['name'] = '/'
+        # else:
+        #     res['name'] = self.number
+        res['journal_id'] = (self.invoice_journal.id or self.session_id.config_id.invoice_journal_id.id)
+        return res
     
      
     numero_doc_relacionado = fields.Char(string='Doc. Relacionado', readonly=True, copy=False)
--- kaf-pos-base/static/js/models.js
+++ kaf-pos-base/static/js/models.js
@@ -4,10 +4,16 @@ odoo.define('kaf-pos-base.models', function(require) {
     var models = require('point_of_sale.models');
     var OrderSuper = models.Order;
     var _posModelSuper = models.PosModel.prototype;
+    var config = require('web.config');
+
+    models.load_fields('pos.config', ['invoice_journal_factura_id', 'invoice_journal_boleta_id', 'invoice_journal_recibo_venta_id','envio_automatico_cpe']);
 
     models.Order = models.Order.extend({
         initialize: function (attributes, options) {
-            var self = this;
+            this.pos = options.pos;
+            this.journal_id_alt_factura = this.pos.config.invoice_journal_factura_id
+            this.journal_id_alt_boleta = this.pos.config.invoice_journal_boleta_id
+            this.journal_id_alt_recibo = this.pos.config.invoice_journal_recibo_venta_id
             this.forma_de_pago_pe_alt = [
                 {'id':0,'code': 'CONTADO', 'name':'CONTADO'},
                 {'id':1,'code': 'CREDITO', 'name':'CRÉDITO'},
@@ -16,6 +22,7 @@ odoo.define('kaf-pos-base.models', function(require) {
             this.to_invoice_factura    = false;
             this.to_invoice_boleta     = false;
             this.to_invoice_recibo     = false;
+            this.journal_id = false; 
             var res = OrderSuper.prototype.initialize.apply(this, arguments);
             return res;
         },
@@ -24,6 +31,8 @@ odoo.define('kaf-pos-base.models', function(require) {
             this.to_invoice_boleta     = false;
             this.to_invoice_recibo     = false;
             this.to_invoice_factura = to_invoice;
+            this.journal_id = this.journal_id_alt_factura; 
+            console.log(this.journal_id)
             this.to_invoice = to_invoice;
         },
         is_to_invoice_factura: function(){
@@ -34,6 +43,8 @@ odoo.define('kaf-pos-base.models', function(require) {
             this.to_invoice_factura    = false;
             this.to_invoice_recibo     = false;
             this.to_invoice_boleta = to_invoice;
+            this.journal_id = this.journal_id_alt_boleta; 
+            console.log(this.journal_id)
             this.to_invoice = to_invoice;
         },
         is_to_invoice_boleta: function(){
@@ -44,13 +55,25 @@ odoo.define('kaf-pos-base.models', function(require) {
             this.to_invoice_factura    = false;
             this.to_invoice_boleta     = false;
             this.to_invoice_recibo = to_invoice;
+            this.journal_id = this.journal_id_alt_recibo; 
+            console.log(this.journal_id)
             this.to_invoice = to_invoice;
         },
         is_to_invoice_recibo: function(){
             return this.to_invoice_recibo;
         },
+        
+        export_as_JSON: function () {
+            var json = OrderSuper.prototype.export_as_JSON.apply(this, arguments);
+            // json['to_invoice_factura'] = this.to_invoice_factura;
+            // json['to_invoice_boleta'] = this.to_invoice_boleta;
+            // json['to_invoice_recibo'] = this.to_invoice_recibo;
+            json['journal_id'] = this.journal_id[0]
+            json['date_invoice'] = moment(new Date().getTime()).format('YYYY/MM/DD');
+            //json['pe_invoice_date']= this.pe_invoice_date;
+            return json;
+        },
     });
-
     // models.load_models([{
     //     model: 'l10n_latam.identification.type',
     //     fields: ["name"],
