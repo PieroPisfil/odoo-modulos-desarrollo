b0bb5d7471003a2293db4fe510417a0c90121234
Piero <piero.pisfil.p@uni.pe>
2022-12-17 00:04:42 -0500

    svsdf

--- kaf-pos-base/static/js/Screens/ReceiptScreen/ReceiptScreen.js
+++ kaf-pos-base/static/js/Screens/ReceiptScreen/ReceiptScreen.js
@@ -12,26 +12,37 @@ odoo.define('kaf-pos-base.ReceiptScreen', function (require) {
             constructor() {
                 super(...arguments);
                 this._state = this.env.pos.TICKET_SCREEN_STATE;
-                this.flag = false
+                this.flag = false;
+                this.id_order = false;
             }
             mounted() {
                 super.mounted();
             }
 
             async buttonImg() {
-                if (!this.actualcurrentOrder()) return;
-                this.currentOrder.finalize();
-                this.showScreen('ReceiptScreen', { order: this.actualcurrentOrder() });
+                //if (!this.actualcurrentOrder()) return;
+                //this.currentOrder.finalize();
+                //this.showScreen('ReceiptScreen', { order: this.actualcurrentOrder() });
+                this.flag = true
+                var ordder = this.currentOrder
+                this.id_order = ordder.pos.validated_orders_name_server_id_map[ordder.name]
+                this.currentOrder.numero_doc_relacionado = await this.order_two().numero_doc_relacionado
+                console.log(this.currentOrder)
+                console.log(this.order_two())
                 this.render();
             }
+           
             async actualcurrentOrder() {
                 //console.log(this.currentOrder)
-                const number_id = this.currentOrder.pos.validated_orders_name_server_id_map[this.currentOrder.name]
-                await this._fetchSyncedOrders(number_id);
-                this.render();
+                
                 //console.log(this._state.syncedOrders.cache[number_id])
                 //return this.currentOrder
-                return this._state.syncedOrders.cache[number_id];
+            }
+            async order_two() {
+                await this._fetchSyncedOrders(this.id_order);
+                //console.log(this.id_order)
+                //console.log(this._state.syncedOrders.cache[this.id_order])
+                return this._state.syncedOrders.cache[this.id_order];
             }
             async _fetchSyncedOrders(number_id) {
                 const fetchedOrders = await this.rpc({
@@ -43,6 +54,15 @@ odoo.define('kaf-pos-base.ReceiptScreen', function (require) {
                 fetchedOrders.forEach((order) => {
                     this._state.syncedOrders.cache[order.id] = new models.Order({}, { pos: this.env.pos, json: order });
                 });
+            }
+            get currentOrder() {
+                return this.env.pos.get_order();
+            }
+            get currentOrderTwo(){
+                if(!this.flag){
+                    return this.env.pos.get_order();
+                }
+                return this.order_two()
             }
         }
     Registries.Component.extend(ReceiptScreen, ReceiptScreenKaf);
